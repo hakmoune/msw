71c0e99cfe85382a7b1a3fa244479c05707d1e46
El Mehdi Hakmoune (EXT) <ehakmoun@cegid.com>
2023-09-15 17:41:39 +0100

    structure and separate code

--- src/mocks/browser.tsx
+++ src/mocks/browser.tsx
@@ -1,45 +1,6 @@
-import { setupWorker, rest } from "msw";
+import { setupWorker } from "msw";
+import { handlers } from "./handlers"
 
-interface Product {
-    id: number;
-    title: string;
-    description: string;
-    price: number;
-    discountPercentage: number;
-    rating: number;
-    stock: number;
-    brand: string;
-    category: string;
-    thumbnail: string;
-    images: string[];
-}
-
-function gitHubApi(path: string): string {
-    return `https://dummyjson.com${path}`
-}
-
-const worker = setupWorker(
-    rest.get<Product>(gitHubApi("/products/1"), (req, res, ctx) => {
-        return res(ctx.json({
-            "id": 1,
-            "title": "iPhone 9",
-            "description": "An apple mobile which is nothing like apple",
-            "price": 549,
-            "discountPercentage": 12.96,
-            "rating": 4.69,
-            "stock": 94,
-            "brand": "Apple",
-            "category": "smartphones",
-            "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-            "images": [
-                "https://i.dummyjson.com/data/products/1/1.jpg",
-                "https://i.dummyjson.com/data/products/1/2.jpg",
-                "https://i.dummyjson.com/data/products/1/3.jpg",
-                "https://i.dummyjson.com/data/products/1/4.jpg",
-                "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
-            ]
-        }))
-    })
-)
+const worker = setupWorker(...handlers)
 
 worker.start()
