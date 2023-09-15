ad49c39661b2a0912d5ba9d9672c5e4efbf1a849
El Mehdi Hakmoune (EXT) <ehakmoun@cegid.com>
2023-09-15 12:33:40 +0100

    Create GET mock server

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+if (process.env.NODE_ENV === 'development')
+  require('./mocks/browser')
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
