fe598cacd335ef3776e0021e38d4fbecff0f38ee
El Mehdi Hakmoune (EXT) <ehakmoun@cegid.com>
2023-09-17 01:42:18 +0100

    add post request in our app

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,16 @@ interface Product {
   images: string[];
 }
 
+interface User {
+  firstName: string,
+  lastName: string,
+  age: number
+}
+
 
 const App: React.FC = () => {
   const [product, setProduct] = useState<Product | null>(null);
+  const [user, setUser] = useState<User>({ firstName: '', lastName: '', age: 0 })
 
   useEffect(() => {
     const param = 1
@@ -34,6 +41,27 @@ const App: React.FC = () => {
 
   }, []);
 
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    fetch("https://dummyjson.com/users/add", {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(user)
+    })
+      .then(response => response.json())
+      .then(console.log)
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    const { name, value } = e.target
+    setUser(state => ({
+      ...state,
+      [name]: value
+    }))
+  }
+
   return (
     <div>
       <h1>Product Details</h1>
@@ -44,6 +72,14 @@ const App: React.FC = () => {
           <p>{product.description}</p>
         </div>
       ) : "loading..."}
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="firstName" value={user.firstName} onChange={handleChange} />
+          <input type="text" name="lastName" value={user.lastName} onChange={handleChange} />
+          <input type="text" name="age" value={user.age} onChange={handleChange} />
+          <button type="submit">Add product</button>
+        </form>
+      </div>
     </div>
   );
 }
